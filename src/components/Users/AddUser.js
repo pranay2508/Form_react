7e1779fa178315f9dcb8791af4c0c70d00810f12
pranay2508 <bhatnagarpranay8@gmail.com>
2023-07-08 20:10:40 +0530

    completed

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 // ../ (DOUBLE DOTS MEANS TO GO UP ONE LEVEL )
+
 import Card from "../UI/Card";
-import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
-const AddUser = (props ) => {
-  const [enteredUsername, setEnteredUsername] = useState(" ");
-  const [enteredAge, setEnteredAge] = useState(" ");
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
-
     //here + means that it will the take the input of age as an (number)
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
-    // console.log(enteredUsername , enteredAge)
-    props.onAddUser(enteredUsername , enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
   };
@@ -29,40 +37,48 @@ const AddUser = (props ) => {
     setEnteredUsername(event.target.value);
   };
 
-  const AgeChangeHandler = (event) => {
+  const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
 
-//   useEffect(()=>{
-// console.log(enteredAge,enteredUsername)
-//   },[enteredUsername,enteredAge])
+  //   useEffect(()=>{
+  // console.log(enteredAge,enteredUsername)
+  //   },[enteredUsername,enteredAge])
 
-
-//react doesn not rendered addjacent components 
+  //react doesn not rendered addjacent components
 
   return (
     <div>
-   <ErrorModal title =" An error occured!" message ="somthing went wrong"/>
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">UserName</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor="age"> Age </label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={AgeChangeHandler}
-        />
-        <Button type="submit"> Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
     </div>
   );
 };
